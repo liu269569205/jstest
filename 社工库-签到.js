001fa0b063275327beb68005f2dee2dae795eaf0
liujingjing <liujingjing@abc.com>
2024-04-18 16:50:06 +0800

    test

--- 社工库-签到.js
+++ 社工库-签到.js
@@ -1,32 +1,21 @@
 //author:https://github.com/yuzd 
-//import axios from "axios";
+const request = require('request');
 
-const axios = require('axios');
-
-// 创建axios实例并配置代理
-/*const axiosInstance = axios.create({
-  proxy: {
-    host: '127.0.0.1',
-    port: 8888
-    // 如果你的代理服务器需要认证，则还需要添加：
-    // auth: {
-    //   username: 'YOUR_PROXY_USERNAME',
-    //   password: 'YOUR_PROXY_PASSWORD'
-    // }
-  }
-});*/
-//https://loseprivacy.xyz  269569205@163.com qwe123
+const jar = request.jar();
+jar.setCookie(request.cookie('USERID=d9h9eilcmv8be1op0fen94sff4'), 'https://loseprivacy.xyz/checkin');
+jar.setCookie(request.cookie('notice=1'), 'https://loseprivacy.xyz/checkin');
+jar.setCookie(request.cookie('_pk_id.29.8a4e=bff527afe8be85b7.1713428749.1.1713428840.1713428749.'), 'https://loseprivacy.xyz/checkin');
+jar.setCookie(request.cookie('_pk_ses.29.8a4e=1'), 'https://loseprivacy.xyz/checkin');
 
 const options = {
   method: 'POST',
   url: 'https://loseprivacy.xyz/checkin',
   headers: {
-    cookie: 'USERID=d9h9eilcmv8be1op0fen94sff4; notice=1; _pk_id.29.8a4e=bff527afe8be85b7.1713428749.1.1713428840.1713428749.; _pk_ses.29.8a4e=1',
     Host: 'loseprivacy.xyz',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
     Accept: 'application/json, text/javascript, */*; q=0.01',
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-   // 'Accept-Encoding': 'gzip, deflate, br',
+    //'Accept-Encoding': 'gzip, deflate, br',
     'X-Requested-With': 'XMLHttpRequest',
     Origin: 'https://loseprivacy.xyz',
     Connection: 'keep-alive',
@@ -34,11 +23,14 @@ const options = {
 	'Sec-Fetch-Dest':'empty',
 	'Sec-Fetch-Mode':'cors',
 	'Sec-Fetch-Site':'same-origin',
-  }
+		cookie: 'USERID=d9h9eilcmv8be1op0fen94sff4; notice=1; _pk_id.29.8a4e=bff527afe8be85b7.1713428749.1.1713428840.1713428749.; _pk_ses.29.8a4e=1',
+  },
+
 };
 
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
 });
+
